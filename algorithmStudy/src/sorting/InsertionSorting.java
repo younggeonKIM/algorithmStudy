c169b452dcf045a9ab2d5b5f985033d398696b6b
영건 김 <young3702@naver.com>
2024-04-25 01:10:08 +0900

    20240425_algorithmStudy(insertionSorting)_added

---
+++ algorithmStudy/src/sorting/InsertionSorting.java
@@ -0,0 +1,40 @@
+package sorting;
+
+public class InsertionSorting {
+
+	public static void main(String[] args) {
+		
+		int [] arr = new int[30];
+		int j, k, temp;
+		for(int i = 0 ; i < arr.length ; i++) {
+			
+			arr[i] = (int)(Math.random()*100);
+		}
+		
+		System.out.println("정렬 전 데이터 : ");
+		for(int i = 0 ; i < arr.length ; i++) {
+			
+			System.out.print(arr[i]+" ");
+		}
+		for(j = 1 ; j < arr.length ; j++) {
+			
+			int target = arr[j];
+			
+			k = j-1;
+			
+			while(k >= 0 && target < arr[k]) {
+				
+				arr[k+1]=arr[k];
+				k--;
+			}
+			arr[k+1] = target;
+		}
+		System.out.println();
+		System.out.println("정렬 후 데이터 (오름차순) : ");
+		for(int i = 0 ; i < arr.length ; i++) {
+
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
