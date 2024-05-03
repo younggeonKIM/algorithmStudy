66517ddca45952a8a3903b38bdd6edb7330ccb09
영건 김 <young3702@naver.com>
2024-05-03 11:38:42 +0900

    20240503_algorithmStudy(quickSort)_updated

--- algorithmStudy/src/sorting/QuickSorting.java
+++ algorithmStudy/src/sorting/QuickSorting.java
@@ -1,12 +1,8 @@
 package sorting;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class QuickSorting {
 
-	public static List<int[]> stdDivideSort(int[] arr, int stdNum) {
+	/*public static List<int[]> stdDivideSort(int[] arr, int stdNum) {
 		
 		int[] newArr;
 		List<int[]> dividedArraysList = new ArrayList<int[]>();
@@ -36,20 +32,62 @@ public class QuickSorting {
 		return dividedArraysList;
 	}
 	
-	
+	public static int getStdNumIdx(int[] arr) {
+		
+		int stdNum = arr.length/2, stdNumidx;
+		
+	}*/
 	
 	public static void main(String[] args) {
 		
 		int[] arr = new int[30];
-		int stdNum, arrLeng=arr.length;
+		int arrLeng=arr.length;
 		
 		for(int i = 0 ; i < arrLeng ; i++) {
 			
 			arr[i] = (int)(Math.random()*100);
 		}
-		
-		stdNum = arr.length/2;
-		
+		System.out.println("정렬 전 데이터 배열 : ");
+		for(int i = 0; i < arrLeng; i++) {
+			
+			System.out.print(arr[i]+" ");
+		}
+		quickSort(arr, 0, arrLeng-1);
+		System.out.println();
+		System.out.println("정렬 전 데이터 배열 : ");
+		for(int i = 0; i < arrLeng; i++) {
+			
+			System.out.print(arr[i]+" ");
+		}
 	}
 
+	public static void quickSort(int arr[], int begin, int end) {
+	    if (begin < end) {
+	        int partitionIndex = partition(arr, begin, end);
+
+	        quickSort(arr, begin, partitionIndex-1);
+	        quickSort(arr, partitionIndex+1, end);
+	    }
+	}
+	
+	private static int partition(int arr[], int begin, int end) {
+	    int pivot = arr[end];
+	    int i = (begin-1);
+
+	    for (int j = begin; j < end; j++) {
+	        if (arr[j] <= pivot) {
+	            i++;
+
+	            int swapTemp = arr[i];
+	            arr[i] = arr[j];
+	            arr[j] = swapTemp;
+	        }
+	    }
+
+	    int swapTemp = arr[i+1];
+	    arr[i+1] = arr[end];
+	    arr[end] = swapTemp;
+
+	    return i+1;
+	}
 }
