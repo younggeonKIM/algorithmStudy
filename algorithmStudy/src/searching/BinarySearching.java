c1757d9b92c18bf4702ab499354b83d0ae0d0167
영건 김 <young3702@naver.com>
2024-05-11 12:02:32 +0900

    20240511_algorithmStudy(binarySearch)_updated

--- algorithmStudy/src/searching/BinarySearching.java
+++ algorithmStudy/src/searching/BinarySearching.java
@@ -2,13 +2,72 @@ package searching;
 
 public class BinarySearching {
 
-	public static void quickSort(int[] arr, int std) {
+	public static void quickSort(int[] arr, int begin, int end) {
 		
+		if (begin < end) {
+	        int partitionIndex = partition(arr, begin, end);
+
+	        quickSort(arr, begin, partitionIndex-1);
+	        quickSort(arr, partitionIndex+1, end);
+	    }
+		/*
+		if((end - begin) / 2  <1) {
+			
+			if(arr[begin] > arr[end]) {
+				
+				int temp = arr[begin];
+				arr[begin]= arr[end];
+				arr[end] =temp;
+			}
+			return;
+		}
+		int pivot = arr[end], arrIdx=begin-1;
+		for(int i = 0 ; i < end ; i++) {
+			
+			if(pivot > arr[i]) {
+				arrIdx++;
+				
+				int temp = arr[i];
+				arr[i] = arr[arrIdx];
+				arr[arrIdx] = temp;
+				arrIdx++;
+			}
+		}
+		int temp = arr[pivot];
+		arr[pivot] = arr[arrIdx+1];
+		arr[arrIdx+1]= temp;
+		
+		
+		
+			quickSort(arr, begin, arrIdx);
+			quickSort(arr, arrIdx+2, end);
+		*/
+	}
+	
+	private static int partition(int arr[], int begin, int end) {
+	    int pivot = arr[end];
+	    int i = (begin-1);
+
+	    for (int j = begin; j < end; j++) {
+	        if (arr[j] <= pivot) {
+	            i++;
+
+	            int swapTemp = arr[i];
+	            arr[i] = arr[j];
+	            arr[j] = swapTemp;
+	        }
+	    }
+
+	    int swapTemp = arr[i+1];
+	    arr[i+1] = arr[end];
+	    arr[end] = swapTemp;
+
+	    return i+1;
 	}
 	
 	public static void binSearch(int[] arr, int start, int end, int target) {
 		
-		int arrLeng = arr.length, middle = (start +end )/2;
+		int middle = (start +end )/2;
 		if(arr[middle] < target) {
 			
 			binSearch(arr, middle +1, end, target);
@@ -30,14 +89,21 @@ public class BinarySearching {
 			
 			arr[i] = (int)(Math.random()*25);
 		}
-		System.out.println("데이터 열의 요소들 : ");
+		System.out.println("정렬 전 데이터 열 : ");
 		for(int i = 0 ; i < arr.length ; i++) {
 			
 			System.out.print(arr[i] + " ");
 		}
 		
+		quickSort(arr, 0, arr.length-1);
+		
+		System.out.println();
+		System.out.println("정렬 후 데이터 열 : ");
+		for(int i = 0 ; i < arr.length ; i++) {
+			
+			System.out.print(arr[i] + " ");
+		}
 		
 		binSearch(arr, 0, 29, targetNum);
 	}
-
 }
